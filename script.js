4209c0d14f668c369f3b15543e520149a9e2c021
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2024-09-23 20:10:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
+	event.preventDefault();
     //Write your code here
+	let fname = document.getElementById('fname');
+	let lname = document.getElementById('lname');
 
+	alert(`${fname.value} ${lname.value}`);
 }
